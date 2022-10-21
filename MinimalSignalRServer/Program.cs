87e959e628abd2e403f6a7f72443b69e374e4aeb
Nathanael Mario Duarte Delfino <nathanael.delfino@live.com>
2022-10-21 14:11:30 -0300

    :up: Subindo fontes necessários do servidor

--- MinimalSignalRServer/Program.cs
+++ MinimalSignalRServer/Program.cs
@@ -1,6 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.SignalR;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.MapHub<MyHub>("/chat");
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+class MyHub : Hub
+{
+    public async IAsyncEnumerable<DateTime> Streaming(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            yield return DateTime.Now;
+            await Task.Delay(1000);
+        }
+    }
+
+}

81b333175f1679c2c9861a9e97c5ded4d4da78c8
Nathanael Mario Duarte Delfino <nathanael.delfino@live.com>
2022-10-21 14:23:25 -0300

    :up: Subindo client que ira consumir o SignalR como cliente

---
+++ MinimalSignalRClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+const string URL = "https://localhost:7140/chat";
+
+await using var connection = new HubConnectionBuilder()
+    .WithUrl(URL)
+    .Build();
+
+await connection.StartAsync();
+
+
+await foreach (var message in connection.StreamAsync<string>("Streaming"))
+{
+    Console.WriteLine(message);
+}
